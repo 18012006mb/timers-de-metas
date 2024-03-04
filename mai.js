5bbdb5cf19de9acce7960bded255401764521406
18012006mb <160506109+18012006mb@users.noreply.github.com>
2024-03-04 11:47:33 -0300

    Add files via upload

---
+++ mai.js
@@ -0,0 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;j++){                   
+   botoes[i].onclick = function(){
+
+         for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo")
+         }
+
+         botoe[i].classlis.add("atio");
+   }
+}
