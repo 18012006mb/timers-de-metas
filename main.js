030c7ffb4311ba80eba45152144441b53f54c6f5
18012006mb <160506109+18012006mb@users.noreply.github.com>
2024-03-15 09:52:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ for(let i=0;i <botoes.length;j++){
             botoes[j].classList.remove("ativo")
          }
 
-         botoes[i].classList.add("atio");
+         botoes[i].classList.add("ativo");
    }
 }
