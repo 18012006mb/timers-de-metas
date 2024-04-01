1b0aa7fe3317a213f33e80dc41cc80e4aa9539d4
18012006mb <160506109+18012006mb@users.noreply.github.com>
2024-04-01 11:37:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -21,5 +21,33 @@ const tempoobjetivo4 = new Date ("2024-10-05T00:00:00");
 
 const tempos = [tempoobjetivo1,tempoobjetivo2,tempoobjetivo3,tempoobjetivo4];
 
-function calculaTempo(tempoobjetivo) {s}
+function calculaTempo(tempoobjetivo) {
 let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Match.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+if (tempoFinal > 0){
+   return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+} else {
+   return "Prazo Finalizado";
+}
+}
+
+function atualizaCronometro(){
+   for (let i=0; i<contadores.length;i++){     
+      contadores [i].textContent = calculaTempo(tempos[i]);                     
+   }
+}
+
+function comecaCronometro(){
+   atualizaCronometro();
+   setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
